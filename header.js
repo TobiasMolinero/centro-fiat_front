251f8e386055b5b31edffc0686c6198a3c21d11d
Tobias Molinero <tobiasmolinero98@gmail.com>
2023-11-15 18:08:17 -0300

    Actualice el header

--- header.js
+++ header.js
@@ -1,67 +1,35 @@
-const itemDesplegableVentas = document.querySelector('#item-desplegable-ventas');
-const itemDesplegableInventario = document.querySelector('#item-desplegable-inventario');
-const menuDesplegableVentas = document.querySelector('.menu-desplegable-ventas');
-const menuDesplegableInventario = document.querySelector('.menu-desplegable-inventario');
+const itemDesplegableInventario = document.querySelector("#item-desplegable-inventario");
 
-let menuDesplegadoVentas = false;
+const menuDesplegableInventario = document.querySelector(".menu-desplegable-inventario");
+
 let menuDesplegadoInventario = false;
 
-window.addEventListener('click', () => {
-    if(menuDesplegadoInventario === true || menuDesplegadoVentas === true){
-        cerrarMenuInventario();
-        cerrarMenuVentas();
-    }
-})
-
-itemDesplegableVentas.addEventListener('click', (e) => {
-    e.stopPropagation();
-    if(menuDesplegadoVentas){
-        cerrarMenuVentas();
-    } else {
-        if(menuDesplegadoInventario){
-            cerrarMenuInventario();
-        }
-        desplegarMenuVentas();
-    }
+window.addEventListener("click", () => {
+  if (menuDesplegadoInventario === true || menuDesplegadoVentas === true) {
+    cerrarMenuInventario();
+    cerrarMenuVentas();
+  }
 });
 
-itemDesplegableInventario.addEventListener('click', (e) => {
-    e.stopPropagation()
-    if(menuDesplegadoInventario){
-        cerrarMenuInventario();
-    } else {
-        if(menuDesplegadoVentas){
-            cerrarMenuVentas();
-        }
-        desplegarMenuInventario();
-    }
+itemDesplegableInventario.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (menuDesplegadoInventario) {
+    cerrarMenuInventario();
+  } else {
+    desplegarMenuInventario();
+  }
 });
-
-
-const desplegarMenuVentas = () => {
-    menuDesplegableVentas.style.opacity = 1;
-    menuDesplegableVentas.style.top = '5px';
-    menuDesplegableVentas.style.left = '85px';
-    menuDesplegadoVentas = true;
-};
-
-const cerrarMenuVentas = () => {
-    menuDesplegableVentas.style.opacity = 0;
-    menuDesplegableVentas.style.top = '-500px';
-    menuDesplegableVentas.style.left = '-500px';
-    menuDesplegadoVentas = false;
-}
 
 const desplegarMenuInventario = () => {
-    menuDesplegableInventario.style.opacity = 1;
-    menuDesplegableInventario.style.top = '5px';
-    menuDesplegableInventario.style.left = '-10px';
-    menuDesplegadoInventario = true;
+  menuDesplegableInventario.style.opacity = 1;
+  menuDesplegableInventario.style.top = "5px";
+  menuDesplegableInventario.style.left = "150px";
+  menuDesplegadoInventario = true;
 };
 
 const cerrarMenuInventario = () => {
-    menuDesplegableInventario.style.opacity = 0;
-    menuDesplegableInventario.style.top = '-500px';
-    menuDesplegableInventario.style.left = '-500px';
-    menuDesplegadoInventario = false;
-}
+  menuDesplegableInventario.style.opacity = 0;
+  menuDesplegableInventario.style.top = "-500px";
+  menuDesplegableInventario.style.left = "-500px";
+  menuDesplegadoInventario = false;
+};
